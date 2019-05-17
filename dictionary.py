4594494b66d292051b29dcedaee96abbd9181060
Edgardo Santiago <edgardoj.santiago@gmail.com>
2019-05-17 14:15:32 -0400

    Uploaded dictionary file
    Added dictionary file

---
+++ dictionary.py
@@ -0,0 +1,35 @@
+jobtype = {
+            "todos":"%","Generales":"Generales","Internados":"Internados",
+            "Otros":"Otros","Profesionales":"Profesionales",
+            "Temporeros":"Temporeros","Ventas":"Ventas"
+            }
+
+pueblos = {
+            "Area Central":"Area+Central", "Area Este":"Area+Este", "Area Metro":"Area+Metro",
+            "AreaNorte":"Area+Norte", "Area Oeste":"Area+Oeste", "Area Sur":"Area+Sur",
+            "Adjuntas":"Adjuntas", "Aguada":"Aguada", "":"Aguadilla":"Aguadilla",
+            "Aguas Buenas":"Aguas Buenas", "Aibonito":"Aibonito", "Anasco":"Anasco",
+            "Arecibo":"Arecibo", "Arroyo":"Arroyo", "Barceloneta":"Barceloneta",
+            "Barranquitas":"Barranquitas", "Bayamon":"Bayamon", "Cabo Rojo":"Cabo+Rojo",
+            "Caguas":"Caguas", "Camuy":"Camuy", "Canovanas":"Canovanas",
+            "Carolina":"Carolina", "Catano":"Catano", "Cayey":"Cayey",
+            "Ceiba":"Ceiba", "Ciales":"Ciales", "Cidra":"Cidra", "Coamo":"Coamo",
+            "Comerio":"Comerio", "Corozal":"Corozal", "Culebra":"Culebra",
+            "Dorado":"Dorado", "Fajardo":"Fajardo", "Florida":"Florida",
+            "Guanica":"Guanica", "Guayama":"Guayama", "Guayanilla":"Guayanilla",
+            "Guaynabo":"Guaynabo", "Gurabo":"Gurabo", "Hatillo":"Hatillo",
+            "Hormigueros":"Hormigueros", "Humacao":"Humacao", "Isabela":"Isabela",
+            "Jayuya":"Jayuya", "Juana Diaz":"Juana+Diaz", "Juncos":"Juncos",
+            "Lajas":"Lajas", "Lares":"Lares", "Las Marias":"Las+Marias",
+            "Las Piedras":"Las+Piedras", "Loiza":"Loiza", "Luquillo":"Luquillo",
+            "Manati":"Manati", "Maricao":"Maricao", "Maunabo":"Maunabo", "Mayaguez":"Mayaguez",
+            "Moca":"Moca", "Morovis":"Morovis", "Naguabo":"Naguabo", "Naranjito":"Naranjito",
+            "Orocovis":"Orocovis", "Patillas":"Patillas", "Penuelas":"Penuelas",
+            "Ponce":"Ponce", "Quebradillas":"Quebradillas", "Rincon":"Rincon",
+            "Rio Grande":"Rio+Grande", "Sabana Grande":"Sabana+Grande", "Salinas":"Salinas",
+            "San German":"San+German", "San Juan":"San+Juan", "San Lorenzo":"San+Lorenzo",
+            "San Sebastian":"San+Sebastian", "Santa Isabel":"Santa+Isabel",
+            "Toa Alta":"Toa+Alta", "Toa Baja":"Toa+Baja", "Trujillo Alto":"Trujillo+Alto",
+            "Utuado":"Utuado", "Vega Alta":"Vega+Alta", "Vega Baja":"Vega+Baja",
+            "Vieques":"Vieques", "Villalba":"Villalba", "Yabucoa":"Yabucoa", "Yauco":"Yauco"
+            }
