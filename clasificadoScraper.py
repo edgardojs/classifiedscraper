e8bf75a45d9903881a1c11eb3dcbc7c1756b0c76
Edgardo Santiago <edgardoj.santiago@gmail.com>
2019-05-18 00:23:22 -0400

    changed the scraper
    changed the scraper to loop through jobs

--- clasificadoScraper.py
+++ clasificadoScraper.py
@@ -4,77 +4,74 @@
 # This will scrape Clasificadosonline.com for jobs
 #
 """
-# Do the imports
+# Do the imports: Dictionary is from the file dictionary.py
+from dictionary import *
 from bs4 import BeautifulSoup as soup
 import requests
 
 # create the scraper class
 class classifiedScraper(object):
-
-
 # Initialization:
 # run your functions here or apply to a global variable object
 # print len and the like to the object can be applied here
-
+# Init right now only got two functions
 
     def __init__(self):
         self._scraper()
         self._file_handler()
 
 # leaving the print statement as a final error check or Done!
-        #print(len(job_anchor_list))
+        print(len(job_list))
 
 # Scraper function to be used on __init__
     def _scraper(self,*args,**kwargs):
-        global page_soup
-        global category_list
-        try:
 
+        global job_list
+
+        try:
             headers = requests.utils.default_headers()
             headers.update({'User-Agent': 'Mozilla/5.0'})
-
-            # Made pueblo a variable
-            # This is here the dictionary imports belong
-            # get user input somehow to get the variables to format.
-            pueblo = "Humacao"
+            pueblo_list = [x for x in pueblos.values()]
 
             # Url Variables
             general_url = "https://www.clasificadosonline.com/UDJobsListing.asp?"
             search_page_url = "https://www.clasificadosonline.com/Jobs.asp"
 
-            # Job Category List
-            # integrate pueblo values after looking in the dictionary
-            # dictionary portion not yet done.
+            try:
+                job_list = []
+                for index, name in enumerate(pueblo_list,start=0):
+                    jobs_cat = "JobsCat=%{}".format(25)
+                    txkey_cat = "&txkey={}".format("")
+                    pueblo_cat = "&Pueblo={}".format(name)
+                    submit_cat = "&Submit=Buscar+-+GO"
+                    offset_cat = "&offset=".format("")
+                    job_search_url = (general_url+jobs_cat+pueblo_cat+txkey_cat+submit_cat+offset_cat)
 
-            jobs_cat = "JobsCat=%{}".format(25)
-            pueblo_cat = "&Pueblo={}".format(pueblo)
-            txkey_cat = "&txkey={}".format("")
-            submit_cat = "&Submit=Buscar+-+GO"
-            offset_cat = "&offset=".format("")
+                    response = requests.get(job_search_url,headers=headers)
+                    page_soup = soup(response.content,"html.parser")
+                    # Applying the soup to some variables
+                    job_anchor_list = page_soup.find_all("td",{"class":"Ver14nounder"})
+                    category_list = page_soup.find_all("select",{"class":"Ver14"})
+                    for item in job_anchor_list:
+                        try:
+                            jobs = item.find("a",{"class":"Ver14nounder"}).get_text()
+                            job_list.append(jobs)
+                        except Exception as e:
+                            print("There was an error: ",e)
+                    # Check & close the response
+                    print("Soup done!", name)
+                    response.close()
+
+            except Exception as e:
+                print("There is an error",e)
 
             # Job search url variable, have tried to do some other type of string
             # formatting but this is the best I could do \ is not working and
             # returning empty objects
-            job_search_url = (general_url + jobs_cat + pueblo_cat + txkey_cat + submit_cat + offset_cat)
-
-            # Requests module stuff scraping done with soup
-            response = requests.get(url),headers=headers)
-            page_soup = soup(response.content,"html.parser")
-
-            category_list = page_soup.find_all("select",{"class":"Ver14"})
-
-            # Stuff yet to implement here for checks and usable Variables
-
-            # print(category_list)
-            # pueblo_list =
-            # job_anchor_list = page_soup.findAll("td",{"class":"Ver14nounder"})
-
-            response.close()
 
         except Exception as e:
             print("There was an error: ",e)
 
-# File Handler function
     def _file_handler(self,*args,**kwargs):
 
 # If you want to use some database use it here.I am using a
@@ -84,23 +81,19 @@ class classifiedScraper(object):
         try:
             file = "jobList.txt"
             f = open(file,"w")
-            Headers = "Company,Job \n"
+            Headers = "Job \n"
             f.write(Headers)
-            for item in category_list:
+            for item in job_list:
                 try:
-                    global jobs
-                    jobs = item.find_all(["option"])
-                    for item in jobs:
-                        item.find(["value"])
-
-                    f.write(str(jobs))
+                    f.write(item + "\n")
                 except Exception as e:
                     print("There was an error: ",e)
+
         except Exception as e:
             print("There was an error:",e)
+
 # running the class
 def main():
-
     classifiedScraper()
 
 # Checking to run main()
